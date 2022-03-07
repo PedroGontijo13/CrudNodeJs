118dd7b827fd3c2acde5710fcc99b93d06e891c4
PedroGont <pedrotolentino2002@gmail.com>
2022-03-07 20:59:27 -0300

    Criacao de POST para usuarios e GET para mostra-los

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'motty.db.elephantsql.com',
+    username: 'zcohnucl',
+    password: 'bxlkbiPWL475Fzhk6lIWAm3Y8L0P7p_M',
+    database: 'zcohnucl',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
