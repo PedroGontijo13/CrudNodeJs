d90031ff3e949f197fa9f9cc63c5746fb27d71c3
PedroGont <pedrotolentino2002@gmail.com>
2022-03-07 23:16:52 -0300

    FinishAPI

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const UserController = require('./controllers/UserControllers')
 
 const routes = express.Router()
 
+
+routes.get('/', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send({ "msg": "This has CORS enabled 🎈" })
+})
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.store)
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
 
+/*
+|\\|LENTAH|\\|
+*/
+
 require('./database/index')
 
 const app = express()
@@ -9,4 +13,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3333)
+app.listen(5555)
